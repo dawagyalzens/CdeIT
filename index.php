1af3dd2e3892e3754e060ee6e13a582cb51201d5
DawaGyalzenSherpa <gyalzensherpa1090@gmail.com>
2019-05-14 00:44:23 +0545

    Index file uploaded

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Swapping Management System</title>
+    <a href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+    <p>This is just a test.</p>
+</body>
+</html>
